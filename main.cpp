4d4e90c97be86f5fcbae098042f027f6d320d3bf
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-13 23:53:41 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,27 @@
 // gmail: <michaelbrockus@gmail.com>
 //
 #include <iostream>
-#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+//
+// Add two numbers and return end result
+//
+int solveMeFirst(const int a, const int b)
+{
+    return a + b;
+} // end of function solveMeFirst
 
 // main is where program execution starts
 int main(void)
 {
-    std::cout << "Lets do some HackerRank stuff" << std::endl;
+    int num1, num2;
+    int sum;
+
+    cin >> num1 >> num2;
+    sum = solveMeFirst(num1, num2);
+    cout << sum << endl;
+
     return EXIT_SUCCESS;
 } // end of function main
