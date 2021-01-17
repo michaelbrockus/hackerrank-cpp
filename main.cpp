137bdfe8d9659884fbdbf9c67941810f61d06bff
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-16 19:30:32 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,39 @@
 //
 #include <iostream>
 #include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+//
+// Add `int max_of_four(int a, int b, int c, int d)` here.
+//
+int maxOfFour(int a, int b, int c, int d)
+{
+    int max = a;
+    if (b > max)
+    {
+        max = b;
+    } // end if
+    if (c > max)
+    {
+        max = c;
+    } // end if
+    if (d > max)
+    {
+        max = d;
+    } // end if
+    return max;
+} // end of function maxOfFour
 
 // main is where program execution starts
-int main(void)
+int main()
 {
-    std::cout << "Lets do some HackerRank stuff" << std::endl;
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    int ans = maxOfFour(a, b, c, d);
+    printf("%d", ans);
+
     return EXIT_SUCCESS;
 } // end of function main
