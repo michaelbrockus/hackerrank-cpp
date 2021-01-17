9a6b74f006681b6a7e0a297632ab214e46183ad5
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-16 16:10:01 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,26 @@
 // gmail: <michaelbrockus@gmail.com>
 //
 #include <iostream>
+#include <vector>
 #include <cstdlib>
 
 // main is where program execution starts
 int main(void)
 {
-    std::cout << "Lets do some HackerRank stuff" << std::endl;
+    vector<int> vect = {0, 0, 0};
+    auto temporary = 0;
+    auto summary = 0;        
+    
+    for (auto iter : vect)
+    {
+        cin >> temporary;
+        vect.push_back(temporary);
+    } // eng for
+    
+    for (auto iter : vect)
+    {
+        summary += iter;
+    } // end for
+    cout << summary << endl;
     return EXIT_SUCCESS;
 } // end of function main
