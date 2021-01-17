0676d6b30a48a82fa1b4b39c2b3e003f9f24cde2
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-16 19:39:31 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,39 @@
 //
 #include <iostream>
 #include <cstdlib>
+#include <cstdio>
+#include <map>
+
+using namespace std;
 
 // main is where program execution starts
-int main(void)
+int main()
 {
-    std::cout << "Lets do some HackerRank stuff" << std::endl;
+    int a, b;
+    cin >> a >> b;
+
+    map<int, string> nums = {
+        {1, "one"}, {2, "two"}, {3, "three"},
+        {4, "four"}, {5, "five"}, {6, "six"},
+        {7, "seven"}, {8, "eight"}, {9, "nine"}
+    };
+
+    for (int i = a; i <= b; ++i)
+    {
+        if (i >= 1 && i <= 9)
+        {
+            auto search = nums.find(i);
+            cout << search->second << endl;
+        } // end if
+        else if (i % 2 == 0)
+        {
+            cout << "even" << endl;
+        } // end else if
+        else
+        {
+            cout << "odd" << endl;
+        } // end else
+
+    } // end for
     return EXIT_SUCCESS;
 } // end of function main
