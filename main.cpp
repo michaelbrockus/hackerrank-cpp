1723ae0b22344b7d17d15081256a36d55b0ae8d5
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-01-16 19:32:44 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,28 @@
 // gmail: <michaelbrockus@gmail.com>
 //
 #include <iostream>
-#include <cstdlib>
+#include <cstdio>
+#include <ios>
+
+using namespace std;
 
 // main is where program execution starts
-int main(void)
+int main()
 {
-    std::cout << "Lets do some HackerRank stuff" << std::endl;
+    int variableInt;
+    long variableLong;
+    char variableChar;
+    float variableFloat;
+    double variableDouble;
+
+    cin >> variableInt >> variableLong >>
+           variableChar >> variableFloat >> variableDouble;
+
+    std::cout << variableInt << std::endl;
+    std::cout << variableLong << std::endl;
+    std::cout << variableChar << std::endl;
+    printf("%.6f\n", variableFloat);
+    printf("%.6lf\n", variableDouble);
+
     return EXIT_SUCCESS;
 } // end of function main
