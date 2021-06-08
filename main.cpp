430f31252851b604b63b13c2e6b13d69fd459e16
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-06-08 07:23:06 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,25 @@
 //
 #include <iostream>
 #include <cstdlib>
+#include <student.hpp>
 
 // main is where program execution starts
 int main(void)
 {
-    std::cout << "Lets do some HackerRank stuff" << std::endl;
+    int age, standard;
+    string first_name, last_name;
+    
+    cin >> age >> first_name >> last_name >> standard;
+    
+    Student st;
+    st.set_age(age);
+    st.set_standard(standard);
+    st.set_first_name(first_name);
+    st.set_last_name(last_name);
+    
+    cout << st.get_age() << endl;
+    cout << st.get_last_name() << ", " << st.get_first_name() << endl;
+    cout << st.get_standard() << endl << endl;
+    cout << st.to_string();
     return EXIT_SUCCESS;
 } // end of function main
