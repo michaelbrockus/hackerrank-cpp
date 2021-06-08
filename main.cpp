ad855e15d70822e91a96394e97766f604a11eaff
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-06-07 23:34:38 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,29 @@
 // author: Michael Brockus
 // gmail: <michaelbrockus@gmail.com>
 //
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
-#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
 
 // main is where program execution starts
 int main(void)
 {
-    std::cout << "Lets do some HackerRank stuff" << std::endl;
+    int value;
+    cin >> value;
+    int arr[value];
+
+    for (int iter = 0; iter < value; ++iter)
+    {
+        scanf("%d", &arr[iter]);
+    } // end for
+
+    for (int iter = value; iter > 0; --iter)
+    {
+        cout << arr[iter - 1] << " ";
+    } // end for
     return EXIT_SUCCESS;
 } // end of function main
